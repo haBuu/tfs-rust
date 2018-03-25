f67e20fa45749e062af73fe507ba6e94271c0c81
haBuu <habuu@habuu.fi>
2018-03-25 12:52:26 +0300

    Use dotenv for DATABASE_URL

--- src/db.rs
+++ src/db.rs
@@ -1,18 +1,18 @@
 extern crate r2d2;
+extern crate dotenv;
+
+use rocket::http::Status;
+use rocket::request::{self, FromRequest};
+use rocket::{Request, State, Outcome};
 
 use diesel::mysql::MysqlConnection;
 use r2d2_diesel::ConnectionManager;
 
 use std::ops::Deref;
-use rocket::http::Status;
-use rocket::request::{self, FromRequest};
-use rocket::{Request, State, Outcome};
+use std::env;
 
 // An alias to the type for a pool of Diesel SQLite connections.
 pub type Pool = r2d2::Pool<ConnectionManager<MysqlConnection>>;
-
-// The URL to the database, set via the `DATABASE_URL` environment variable.
-static DATABASE_URL: &'static str = env!("DATABASE_URL");
 
 // Connection request guard type: a wrapper around an r2d2 pooled connection.
 pub struct DB(pub r2d2::PooledConnection<ConnectionManager<MysqlConnection >>);
@@ -43,6 +43,8 @@ impl Deref for DB {
 
 // Initializes a database pool.
 pub fn init_pool() -> Pool {
-  let manager = ConnectionManager::<MysqlConnection>::new(DATABASE_URL);
+  dotenv::dotenv().ok();
+  let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+  let manager = ConnectionManager::<MysqlConnection>::new(database_url);
   r2d2::Pool::new(manager).expect("db pool")
 }
